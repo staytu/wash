09a8ffb3660000c53b6ff751d81d8402adfb9eb9
evegneka <ana.shestunova@yandex.ru>
2024-06-11 12:49:23 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('a[href^="#"]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 600);
+        return false;
+    })    
+});
+
